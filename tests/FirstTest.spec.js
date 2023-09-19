d2c25e14a7c3c197d69cc90b0e1885e6e3bbc27c
umar <umarmumtaz.info@gmail.com>
2023-09-19 16:02:57 +0500

    m

--- tests/FirstTest.spec.js
+++ tests/FirstTest.spec.js
@@ -1,7 +1,6 @@
 const {test}=require('@playwright/test')
 test('first test case playwright',async ({browser})=>
 {
-
 //javascript is based on async, so to use hold on, that is await with every step
 //if there is no fuction name is call aynonomous function, and just b like [async()=>] instad of async function()
 // add the global fixture that is acync({browser})-fixtures are nothting but the global variables
@@ -9,5 +8,4 @@ chrome - Plugins/ cookies
 const context =await browser.newContext(); //remove the cookies
 const page= await context.newPage(); //newpage method, 
 await page.goto("https://test.jobtrain.co.uk/ybscareers/Home/Job");
-
 });
