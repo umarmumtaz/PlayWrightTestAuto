e95424e12610df776d107533d9e1e8aa1c3066e0
umar <umarmumtaz.info@gmail.com>
2023-10-06 01:41:52 +0500

    m

--- tests/FirstTest.spec.js
+++ tests/FirstTest.spec.js
@@ -1,5 +1,7 @@
 const {test, expect}=require('@playwright/test')
-test.only('first test case playwright',async ({page})=>
+
+
+test('Ybs Signin with happy scenario',async ({page})=>
 {
 //javascript is based on async, so to use hold on, that is await with every step
 //if there is no fuction name is call aynonomous function, and just b like [async()=>] instad of async function()
@@ -8,10 +10,28 @@ test.only('first test case playwright',async ({page})=>
 // const context =await browser.newContext(); //remove the cookies
 // const page= await context.newPage(); //newpage method, 
 //npx playwright test --headed
-await page.goto("https://test.jobtrain.co.uk/ybscareers/Home/Job");
+await page.goto("c");
 console.log(await page.title()); 
 await expect(page).toHaveTitle("Jobs Page - YBS Careers | Jobs | Search here for your perfect career");
 await page.getByLabel('Allow cookies').click();
 await page.getByRole('link', { name: 'Sign in' }).click();
+await page.locator('#signInEmail').type('nanncy@gmail.com')
+await page.locator('#inputPassword').type('Testing@123')
+await page.locator('#signIn').click();
+});         
 
-});     
+
+
+test.only('ybs Login with -scenarios',async({page})=>{
+    await page.goto("https://test.jobtrain.co.uk/ybscareers/Home/Job");
+    console.log(await page.title()); 
+    await expect(page).toHaveTitle("Jobs Page - YBS Careers | Jobs | Search here for your perfect career");
+    await page.getByLabel('Allow cookies').click();
+    await page.getByRole('link', { name: 'Sign in' }).click();
+    await page.locator('#signInEmail').type('')
+    await page.locator('#inputPassword').type('')
+    await page.locator('#signIn').click();
+    console.log(await page.locator('.alert-danger').textContent());
+    await expect(page.locator('.alert-danger')).toContainText('Invalid UserName Or Password.');
+
+});
