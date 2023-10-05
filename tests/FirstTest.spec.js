44bc79faf76c3b9db7b23ceb45a79f631ef012af
umar <umarmumtaz.info@gmail.com>
2023-10-06 00:00:45 +0500

    m

--- tests/FirstTest.spec.js
+++ tests/FirstTest.spec.js
@@ -1,5 +1,5 @@
-const {test}=require('@playwright/test')
-test('first test case playwright',async ({browser,page})=>
+const {test, expect}=require('@playwright/test')
+test('first test case playwright',async ({page})=>
 {
 //javascript is based on async, so to use hold on, that is await with every step
 //if there is no fuction name is call aynonomous function, and just b like [async()=>] instad of async function()
@@ -7,5 +7,8 @@ test('first test case playwright',async ({browser,page})=>
 // chrome - Plugins/ cookies
 // const context =await browser.newContext(); //remove the cookies
 // const page= await context.newPage(); //newpage method, 
+//npx playwright test --headed
 await page.goto("https://test.jobtrain.co.uk/ybscareers/Home/Job");
-});  
+console.log(await page.title()); 
+await expect(page).toHaveTitle("Jobs Pagne - YBS Careers | Jobs | Search here for your perfect career");
+});     
