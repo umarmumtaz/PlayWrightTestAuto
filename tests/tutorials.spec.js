853d93cffa1c3b27f5006714b4fdb8c986cf1bee
umar <umarmumtaz.info@gmail.com>
2023-10-26 16:29:09 +0500

    m

--- tests/tutorials.spec.js
+++ tests/tutorials.spec.js
@@ -5,3 +5,10 @@
 //  expect  https://elaichenkov.github.io/playwright-expect/
 
 
+//const locator = page.locator('input');
+//await expect(locator).toHaveAttribute('class', '<blinking text i.e class name>');
+  
+//handle child win https://medium.com/@kbalaji.kks/playwright-handling-alert-prompt-new-window-new-tab-91b8377c9b03
+//https://www.freecodecamp.org/news/guide-to-javascript-promises/
+
+
