263b0456f271f7e97b2c92e2744dd5fb8ed8a6d3
umar <umarmumtaz.info@gmail.com>
2023-11-28 20:46:59 +0500

    m

--- tests/pageobjects/ClientappPO.spec.js
+++ tests/pageobjects/ClientappPO.spec.js
@@ -1,15 +1,13 @@
 const { test, expect, request } = require('@playwright/test');
+const {LoginPage} =require('./LoginPage')
 
 test('Verify the login with Page object', async ({ page }) => {
-    await page.goto('https://test.jobtrain.co.uk/ybscareers/')
+    const username = "nanncykevin@gmail.com";
+    const password = "Testing@123";
+    const loginPage =new LoginPage(page);
+    loginPage.goTo();
+    loginPage.validLogin(username,password);
     await page.locator('.sign_in_detail').click()
-    await page.getByPlaceholder('Email goes here').type('nanncy5@gmail.com')
-    await page.getByPlaceholder('Password').type('Testing@123')
-    await page.locator('#signIn').click()
-    await expect(page).toHaveURL(/MyJobs/);
-    await page.getByText('Applications', 'Saved')
-    await page.getByRole('button').click()
-
     await page.pause()
 });
 
--- tests/pageobjects/LoginPage.js
+++ tests/pageobjects/LoginPage.js
@@ -1,6 +1,27 @@
 class LoginPage{
-constructor()
+constructor(page)
 {
-    
+this.page=page;
+this.signInbutton = page.locator('#signIn');
+this.userName = page.getByPlaceholder('Email goes here');
+this.password = page.getByPlaceholder('Password');
+
+}
+
+async goTo()
+{
+
+    await this.page.goto("https://test.jobtrain.co.uk/ybscareers/"); 
+
+}
+
+
+async validLogin(username, password)
+{
+    await this.userName.type(username)
+    await this.password.type(password)
+    await this.signInbutton.click()
+
 }
 }
+module.exports = {LoginPage};
