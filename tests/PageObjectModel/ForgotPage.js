bd2d1b1a5b2f676eb23e53293de2b20e39768358
umar <umarmumtaz.info@gmail.com>
2023-12-01 01:20:03 +0500

    m

--- tests/PageObjectModel/ForgotPage.js
+++ tests/PageObjectModel/ForgotPage.js
@@ -1,12 +1,21 @@
-exports.ForgotPage = class ForgotPage {
+const { expect } = require("@playwright/test");
 
+exports.ForgotPage = class ForgotPage {
     constructor(page) {
         this.page = (page);
         this.signIn = page.locator('.sign_in_detail');
-        this.forgotbutton = page.getByRole('link', { name: 'I forgot my password' })
+        this.forgotbutton = page.getByRole('link', { name: 'I forgot my password' });
+        this.recoverPassword=page.locator('#RecoverPassword');
+       // this.resetPassword =page.locator('#signInEmail').fill('nanncy@gmail.com')
+        //this.submit=page.locator('.btn-primary')
     }
     async Forgot() {
         await this.signIn.click()
         await this.forgotbutton.click()
+        await this.recoverPassword.click()
+       // await this.submit.click()
+       //await expect (page.locator('.register__title').textContent()).toEqual('Reset Password')
+       //  expect(await page.locator('.register__title').textContent()).toEqual('Reset Password')
+       
     }
 }
