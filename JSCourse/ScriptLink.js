4b7675d9acf7daf774a5b82352646f106c931455
umar <umarmumtaz.info@gmail.com>
2024-01-10 17:18:52 +0500

    m

---
+++ JSCourse/ScriptLink.js
@@ -0,0 +1,22 @@
+
+const jsNature = 'amazing';
+if (jsNature === 'amazing') alert("JS is amazing")
+console.log(40 + 8 + 21)
+console.log("paul")
+let studentName ="Nanncy";
+console.log(studentName)
+//first assignment
+let country ='Pakistan';
+let continent='Asia';
+let population='30cr';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let ourCountryIsPakistan =true;
+console.log(ourCountryIsPakistan)
+console.log(typeof ourCountryIsPakistan)
+ourCountryIsPakistan ='yes'
+console.log(typeof ourCountryIsPakistan)
+ourCountryIsPakistan =740
+console.log(typeof ourCountryIsPakistan)
