5600cdf4364b44bb7b0725d2b005dc70e4a3b042
umar <umarmumtaz.info@gmail.com>
2024-02-10 07:18:59 +0500

    m

--- tests/singleTest.spec.js
+++ tests/singleTest.spec.js
@@ -4,19 +4,23 @@ const exp = require('constants');
 
 test('Prctise', async ({ page }) => {
     await page.goto("https://test.jobtrain.co.uk/ybscareers/Home/Job");
-    
-    const response =await page.request.get('https://test.jobtrain.co.uk/ybscareers/Home/Job')
+
+    const response = await page.request.get('https://test.jobtrain.co.uk/ybscareers/Home/Job')
     await expect(response).toBeOK()
+    console.log(response)
     await expect(page).toHaveURL(/Job/)
+
+    const valueVar = { output: '200' }
+    expect(200).toBeTruthy();
+    console.log(valueVar)
 
     const homePage = await page.innerText('.h3.jt-page-title');
     expect(homePage).toBe('Jobs for you')
     console.log(homePage)
-    
-    console.log(response)
+
     console.log(await page.title());
     page.pause()
-    
+
 });
 
 
