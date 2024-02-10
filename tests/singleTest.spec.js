c89742c4dc9dccc2fb63ee80fe36006a0b739d4f
umar <umarmumtaz.info@gmail.com>
2024-02-11 03:57:01 +0500

    m

--- tests/singleTest.spec.js
+++ tests/singleTest.spec.js
@@ -4,19 +4,21 @@ const exp = require('constants');
 
 test('Prctise', async ({ page }) => {
     await page.goto("https://test.jobtrain.co.uk/ybscareers/Home/Job");
-
+    //API requests like get, also check the url generic assertions
     const response = await page.request.get('https://test.jobtrain.co.uk/ybscareers/Home/Job')
     await expect(response).toBeOK()
     console.log(response)
     await expect(page).toHaveURL(/Job/)
-
+    //verify the response code with truthy/falsy
     const valueVar = { output: '200' }
     expect(200).toBeTruthy();
     console.log(valueVar)
-
+    //verify the page title with assertion like tocontain
     const homePage = await page.innerText('.h3.jt-page-title');
     expect(homePage).toBe('Jobs for you')
     console.log(homePage)
+    expect(homePage).toContain('Jobs')
+    expect(homePage).toHaveLength(12)
 
     console.log(await page.title());
     page.pause()
