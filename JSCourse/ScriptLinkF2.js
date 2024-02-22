606f31f02cfaa84f9c9bdb1cbb4d5538d99f43e3
umar <umarmumtaz.info@gmail.com>
2024-02-23 04:27:23 +0500

    m

--- JSCourse/ScriptLinkF2.js
+++ JSCourse/ScriptLinkF2.js
@@ -21,5 +21,19 @@ function fruitProcessing(apple, berry) {
 const applejuice = fruitProcessing(5, 0)
 console.log(applejuice);
 
-const applyBerryJuice = fruitProcessing(1,5)
+const applyBerryJuice = fruitProcessing(1, 5)
 console.log(applyBerryJuice)
+
+
+//function declaration
+function age1(birthday) {
+    return 2037 - birthday
+}
+const calAge = age1(1991)
+console.log(calAge)
+//function expressions
+const cal2 = function(birthday){
+    return 2040 -birthday;
+}
+ const age2 = cal2(1991)
+console.log(age2, age1)
