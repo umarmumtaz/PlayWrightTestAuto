de11b572c972525e6f9228a913d128458e326cd1
umar <umarmumtaz.info@gmail.com>
2024-02-25 05:09:38 +0500

    m

--- JSCourse/ScriptLinkF2.js
+++ JSCourse/ScriptLinkF2.js
@@ -14,7 +14,7 @@ function test1() {
 test1();
 
 function fruitProcessing(apple, berry) {
-    console.log(apple, berry)
+    // console.log(apple, berry)
     const juice = ('juice with' + apple + 'and' + berry)
     return juice;
 }
@@ -31,9 +31,65 @@ function age1(birthday) {
 }
 const calAge = age1(1991)
 console.log(calAge)
+
 //function expressions
-const cal2 = function(birthday){
-    return 2040 -birthday;
+const cal2 = function (birthday) {
+    return 2040 - birthday;
 }
- const age2 = cal2(1991)
+const age2 = cal2(1991)
 console.log(age2, age1)
+
+/////----------------------------Practice GPT
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("John")); // Output: "Hello, John!"
+
+function testV(name1) {
+    const nameT = ('Hello,' + name1)
+    return (nameT)
+} console.log(testV('Umar'))
+
+function testV1(name2) {
+    return name2
+} console.log(testV1('umar'))
+
+function addTest(add) {
+    return add
+} console.log(addTest(10 + 96))
+
+function addTest2(a, b) {
+    return a + b
+} console.log(addTest2(10, 8));
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }}
+console.log(factorial(5)); // Output: 120
+
+
+
+//arrow functions 
+const ageTest2 = brithday1 =>2024 - brithday1
+const ageTest3 =ageTest2
+console.log(ageTest3(1998))
+
+///calculate year of retirements
+const retirementTest = (birthdayr, namere) =>{
+const ageTestr = 2024-birthdayr
+const retirementlimit = 65 - ageTestr
+return ('retiring time is'+ retirementlimit+'for'+ namere)
+}
+console.log(retirementTest(1988, "Umar" ))
+
+
+const TotlaJob = (joinintdate) =>{
+const bbb = 2024-joinintdate
+return(bbb)
+
+}
+console.log(TotlaJob(2015))
